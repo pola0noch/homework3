c6447de2b1c2beaa5c9bdf3ca9cf76966f0fe782
pola0noch <klayriddell@gmail.com>
2024-07-03 15:10:37 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+# урок по теме "Динамическая типизация"
+name = "Kate"
+print("Name:",name)
+age = 24
+print("Age:",age)
+age = age + 1
+print("New age:",age)
+is_student = True
+print("Is student:",is_student)
